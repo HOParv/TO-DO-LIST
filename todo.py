5a5ffea0fad2ede0b4cd7b99892f381ae178db3c
Xero13 <javierpescadornunez@gmail.com>
2026-04-24 21:54:47 +0200

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,11 +1,15 @@
 print("TO-DO LIST")
-print("To exit press ctrl + c")
+print("To exit write exit")
 tarea = []
 
 while True:
     ntarea = input("\033[31mPlease write your tasks here:\033[0m ")
 
-    tarea.append(ntarea)
-    print("\033[34m CURRENT LIST \033[0m")
-    for item in tarea:
-        print(item)
+    if ntarea == "exit":
+        print("goodbye")
+        break
+    else:
+        tarea.append(ntarea)
+        print("\033[34m CURRENT LIST \033[0m")
+        for item in tarea:
+            print(item)
